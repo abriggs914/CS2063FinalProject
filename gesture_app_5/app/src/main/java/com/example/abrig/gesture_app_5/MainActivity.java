3638fefc436881281db92f1e22f9efa84e9af3f8
Avery Briggs <abriggs914@gmail.com>
2019-03-19 16:04:06 -0300

    2019-03-19 Created some sample gesture apps to get familiar with the package

---
+++ gesture_app_5/app/src/main/java/com/example/abrig/gesture_app_5/MainActivity.java
@@ -0,0 +1,127 @@
+package com.example.abrig.gesture_app_5;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView gestureText;
+    private GestureDetector myGestureDectector;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        gestureText = (TextView) findViewById(R.id.tvGesture);
+        AndroidGestureDectector androidGestureDectector = new AndroidGestureDectector();
+        myGestureDectector = new GestureDetector(MainActivity.this, androidGestureDectector);
+
+    }
+
+    class AndroidGestureDectector implements GestureDetector.OnGestureListener, GestureDetector.OnDoubleTapListener {
+
+        @Override
+        public boolean onSingleTapConfirmed(MotionEvent motionEvent) {
+            gestureText.setText("onSingleTapConfirmed");
+            Log.d("Gesture ", "onSingleTapConfirmed");
+            return false;
+        }
+
+        @Override
+        public boolean onDoubleTap(MotionEvent motionEvent) {
+            gestureText.setText("onDoubleTap");
+            Log.d("Gesture ", "onDoubleTap");
+            return false;
+        }
+
+        @Override
+        public boolean onDoubleTapEvent(MotionEvent motionEvent) {
+            gestureText.setText("onDoubleTapEvent");
+            Log.d("Gesture ", "onDoubleTapEvent");
+            return false;
+        }
+
+        @Override
+        public boolean onDown(MotionEvent motionEvent) {
+            gestureText.setText("onDown");
+            Log.d("Gesture ", "onDown");
+            return false;
+        }
+
+        @Override
+        public void onShowPress(MotionEvent motionEvent) {
+            gestureText.setText("onShowPress");
+            Log.d("Gesture ", "onShowPress");
+
+        }
+
+        @Override
+        public boolean onSingleTapUp(MotionEvent motionEvent) {
+            gestureText.setText("onSingleTapUp");
+            Log.d("Gesture ", "onSingleTapUp");
+            return false;
+        }
+
+        @Override
+        public boolean onScroll(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
+            gestureText.setText("onScroll");
+            if (motionEvent.getX() < motionEvent1.getX()) {
+                Log.d("Gesture ", "Left to Right Scroll: " + motionEvent.getX() + " - " + motionEvent1.getX());
+                Log.d("Speed ", String.valueOf(v) + " pixels/second");
+            }
+            if (motionEvent.getX() > motionEvent1.getX()) {
+                Log.d("Gesture ", "Right to Left Scroll: " + motionEvent.getX() + " - " + motionEvent1.getX());
+                Log.d("Speed ", String.valueOf(v) + " pixels/second");
+            }
+            if (motionEvent.getY() < motionEvent1.getY()) {
+                Log.d("Gesture ", "Up to Down Scroll: " + motionEvent.getY() + " - " + motionEvent1.getY());
+                Log.d("Speed ", String.valueOf(v1) + " pixels/second");
+            }
+            if (motionEvent.getY() > motionEvent1.getY()) {
+                Log.d("Gesture ", "Down to Up Scroll: " + motionEvent.getY() + " - " + motionEvent1.getY());
+                Log.d("Speed ", String.valueOf(v1) + " pixels/second");
+            }
+            return false;
+        }
+
+        @Override
+        public void onLongPress(MotionEvent motionEvent) {
+            gestureText.setText("onLongPress");
+            Log.d("Gesture ", "onLongPress");
+
+        }
+
+        @Override
+        public boolean onFling(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
+            gestureText.setText("onFling");
+            if (motionEvent.getX() < motionEvent1.getX()) {
+                Log.d("Gesture ", "Left to Right Fling: " + motionEvent.getX() + " - " + motionEvent1.getX());
+                Log.d("Speed ", String.valueOf(v) + " pixels/second");
+            }
+            if (motionEvent.getX() > motionEvent1.getX()) {
+                Log.d("Gesture ", "Right to Left Fling: " + motionEvent.getX() + " - " + motionEvent1.getX());
+                Log.d("Speed ", String.valueOf(v) + " pixels/second");
+            }
+            if (motionEvent.getY() < motionEvent1.getY()) {
+                Log.d("Gesture ", "Up to Down Fling: " + motionEvent.getY() + " - " + motionEvent1.getY());
+                Log.d("Speed ", String.valueOf(v1) + " pixels/second");
+            }
+            if (motionEvent.getY() > motionEvent1.getY()) {
+                Log.d("Gesture ", "Down to Up Fling: " + motionEvent.getY() + " - " + motionEvent1.getY());
+                Log.d("Speed ", String.valueOf(v1) + " pixels/second");
+            }
+            return false;
+        }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        myGestureDectector.onTouchEvent(event);
+        return super.onTouchEvent(event);
+    }
+}
